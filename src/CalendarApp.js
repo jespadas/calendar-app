2690676fa8dced50f115cb493a711ab9862ed3cf
jespadas <julio.espadas@gmail.com>
2021-10-05 14:40:22 +0200

    app root and routing

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { AppRouter } from './routers/AppRouter'
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
+
+import { AppRouter } from './routers/AppRouter';
 
 export const CalendarApp = () => {
-    return (
-        <div>
-            <AppRouter />
-        </div>
-    )
-}
+	return (
+		<Provider store={store}>
+			<AppRouter />
+		</Provider>
+	);
+};
--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CalendarApp />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './styles.css';
+
+ReactDOM.render(<CalendarApp />, document.getElementById('root'));
