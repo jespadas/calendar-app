7ccbdd4d0a00ea47e3542be77db5764db16e60ec
jespadas <julio.espadas@gmail.com>
2021-10-05 14:41:02 +0200

    types and reducers

---
+++ src/types/types..js
@@ -0,0 +1,12 @@
+export const types = {
+	// UI
+	uiOpenModal: '[UI] Open modal',
+	uiCloseModal: '[UI] Close modal',
+	// EVENTS
+	eventSetActive: '[EVENT] Set active',
+	eventAddNew: '[EVENT] Add new event',
+	eventClearActiveEvent: '[EVENT] Clear active event',
+	eventUpdate: '[EVENT] Update event',
+	eventDelete: '[EVENT] Delete event',
+
+};
