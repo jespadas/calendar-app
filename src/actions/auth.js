0c49111c1c4f3f57cac62f851b790790f084b8c1
jespadas <julio.espadas@gmail.com>
2021-10-07 16:45:22 +0200

    login and register actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,6 @@
 import Swal from 'sweetalert2';
 
-import { fetchWithoutToken } from '../helpers/fetch';
+import { fetchWithoutToken, fetchWithToken } from '../helpers/fetch';
 import { types } from '../types/types.';
 
 export const startLogin = (email, password) => {
@@ -53,7 +53,43 @@ export const startRegister = (email, password, name) => {
 	};
 };
 
+export const startChecking = () => {
+	return async (dispatch) => {
+		const resp = await fetchWithToken('auth/renew');
+		const body = await resp.json();
+
+		if (body.ok) {
+			localStorage.setItem('token', body.token);
+			localStorage.setItem('token-init-date', new Date().getTime());
+
+			dispatch(
+				login({
+					uid: body.uid,
+					name: body.name,
+				})
+			);
+		} else {
+			dispatch(checkingFinish());
+		}
+	};
+};
+
+const checkingFinish = () => ({
+	type: types.authFinishChecking,
+});
+
 const login = (user) => ({
 	type: types.authLogin,
 	payload: user,
+});
+
+export const StartLogout = () => {
+	return (dispatch) => {
+		localStorage.clear();
+		dispatch(logout());
+	};
+};
+
+const logout = () => ({
+	type: types.authLogout,
 });
