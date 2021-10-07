47534946af8e86fbbd9e00512534b5c903c4f24b
jespadas <julio.espadas@gmail.com>
2021-10-07 15:30:41 +0200

    auth actions

---
+++ src/actions/auth.js
@@ -0,0 +1,59 @@
+import Swal from 'sweetalert2';
+
+import { fetchWithoutToken } from '../helpers/fetch';
+import { types } from '../types/types.';
+
+export const startLogin = (email, password) => {
+	return async (dispatch) => {
+		const resp = await fetchWithoutToken(
+			'auth/login',
+			{ email, password },
+			'POST'
+		);
+		const body = await resp.json();
+
+		if (body.ok) {
+			localStorage.setItem('token', body.token);
+			localStorage.setItem('token-init-date', new Date().getTime());
+
+			dispatch(
+				login({
+					uid: body.uid,
+					name: body.name,
+				})
+			);
+		} else {
+			Swal.fire('Error', body.msg, 'error');
+		}
+	};
+};
+
+export const startRegister = (email, password, name) => {
+	return async (dispatch) => {
+		const resp = await fetchWithoutToken(
+			'auth/register',
+			{ name, email, password },
+			'POST'
+		);
+		const body = await resp.json();
+
+		if (body.ok) {
+			localStorage.setItem('token', body.token);
+			localStorage.setItem('token-init-date', new Date().getTime());
+
+			dispatch(
+				login({
+					uid: body.uid,
+					name: body.name,
+				})
+			);
+		} else {
+			Swal.fire('Error', body.msg, 'error');
+		}
+	};
+};
+
+const login = (user) => ({
+	type: types.authLogin,
+	payload: user,
+});
